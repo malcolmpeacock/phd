cf9f6715c211b564a61573056d72ccb610b243db
malcolmpeacock <malcolm@popmalc.org.uk>
2021-02-04 09:17:08 +0000

    simple ann example

---
+++ challenge/kaggle_simple_example.py
@@ -0,0 +1,79 @@
+# Imports
+import torch.nn as nn
+import torch
+import numpy as np
+# Import tensor dataset & data loader
+from torch.utils.data import TensorDataset, DataLoader
+# Import nn.functional
+import torch.nn.functional as F
+
+class SimpleNet(nn.Module):
+    # Initialize the layers
+    def __init__(self):
+        super().__init__()
+        self.linear1 = nn.Linear(3, 3)
+        self.act1 = nn.ReLU() # Activation function
+        self.linear2 = nn.Linear(3, 2)
+    
+    # Perform the computation
+    def forward(self, x):
+        x = self.linear1(x)
+        x = self.act1(x)
+        x = self.linear2(x)
+        return x
+
+# Input (temp, rainfall, humidity)
+inputs = np.array([[73, 67, 43], [91, 88, 64], [87, 134, 58], [102, 43, 37], [69, 96, 70], [73, 67, 43], [91, 88, 64], [87, 134, 58], [102, 43, 37], [69, 96, 70], [73, 67, 43], [91, 88, 64], [87, 134, 58], [102, 43, 37], [69, 96, 70]], dtype='float32')
+# Targets (apples, oranges)
+targets = np.array([[56, 70], [81, 101], [119, 133], [22, 37], [103, 119], 
+                    [56, 70], [81, 101], [119, 133], [22, 37], [103, 119], 
+                    [56, 70], [81, 101], [119, 133], [22, 37], [103, 119]], dtype='float32')
+
+inputs = torch.from_numpy(inputs)
+targets = torch.from_numpy(targets)
+
+train_ds = TensorDataset(inputs, targets)
+train_ds[0:3]
+
+# Define data loader
+batch_size = 5
+train_dl = DataLoader(train_ds, batch_size, shuffle=True)
+next(iter(train_dl))
+
+# Define model
+# model using regression
+#model = nn.Linear(3, 2)
+#print(model.weight)
+#print(model.bias)
+# model using ANN
+model = SimpleNet()
+
+# Define optimizer
+opt = torch.optim.SGD(model.parameters(), lr=1e-5)
+
+
+# Define loss function
+loss_fn = F.mse_loss
+
+loss = loss_fn(model(inputs), targets)
+print(loss)
+
+# Define a utility function to train the model
+def fit(num_epochs, model, loss_fn, opt):
+    for epoch in range(num_epochs):
+        for xb,yb in train_dl:
+            # Generate predictions
+            pred = model(xb)
+            loss = loss_fn(pred, yb)
+            # Perform gradient descent
+            loss.backward()
+            opt.step()
+            opt.zero_grad()
+        print('epoch {}, loss {}'.format(epoch, loss.item()))
+    print('Training loss: ', loss_fn(model(inputs), targets))
+
+# Train the model for 100 epochs
+fit(100, model, loss_fn, opt)
+preds = model(inputs)
+print(preds)
+print(targets)
