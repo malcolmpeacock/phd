cdcff13f21395feab03f8267cc13359777a7ff9b
malcolmpeacock <malcolm@popmalc.org.uk>
2021-03-08 17:07:27 +0000

    comp df

--- challenge/compare_demand_forecasts.py
+++ challenge/compare_demand_forecasts.py
@@ -31,44 +31,35 @@ input_dir = "/home/malcolm/uclan/challenge/input/"
 output_dir = "/home/malcolm/uclan/challenge/output/"
 
 # demand data
+
+demand_names = ['w1', 'w0', 'regm']
 demand_dfs={}
-glob_filename = '{}demand_forecast_*_{}.csv'.format(output_dir, dataset)
-for filename in glob.glob(glob_filename):
-    method = filename[53:-4]
-    print(filename, method)
+for name in demand_names:
+    filename = '{}demand_forecast_{}_{}.csv'.format(output_dir, name, dataset)
+    print(filename, name)
     df = pd.read_csv(filename, header=0, sep=',', parse_dates=[0], index_col=0, squeeze=True)
     df = df[(df['k']>31) & (df['k']<43)]
-    demand_dfs[method] = df
-    print(demand_dfs[method])
-
-sdays_name = 'sdays_{}'.format(dataset)
-nreg_name = 'nreg_{}'.format(dataset)
-regl_name = 'regl_{}'.format(dataset)
-regm_name = 'regm_{}'.format(dataset)
-demand_dfs[sdays_name].index=range(len(demand_dfs[sdays_name]))
-demand_dfs[nreg_name].index=range(len(demand_dfs[nreg_name]))
-demand_dfs[regl_name].index=range(len(demand_dfs[regl_name]))
-demand_dfs[regm_name].index=range(len(demand_dfs[regm_name]))
+    df.index=range(len(df))
+    demand_dfs[name] = df
+    print(demand_dfs[name])
 
 # plot 
 if args.plot:
-
-    demand_dfs[sdays_name]['demand'].plot(label='actual demand', color='red')
-    demand_dfs[sdays_name]['prediction'].plot(label='10 similar days', color='blue')
-    demand_dfs[nreg_name]['prediction'].plot(label='Regression #3', color='green')
-    demand_dfs[regl_name]['prediction'].plot(label='Regression #1', color='orange')
-    demand_dfs[regm_name]['prediction'].plot(label='Regression #2', color='purple')
+    count=0
+    for name, pdf in demand_dfs.items():
+        print(name)
+        print(pdf)
+        if count==0:
+            pdf['demand'].plot(label='actual demand')
+        pdf['prediction'].plot(label=name)
+        count+=1
+            
     plt.title('Comparison of demand forecasts')
     plt.xlabel('Hour of the year', fontsize=15)
     plt.ylabel('Demand (MWh)', fontsize=15)
     plt.legend(loc='upper right', fontsize=15)
     plt.show()
 
-print('Sdays')
-utils.print_metrics(demand_dfs[sdays_name]['demand'], demand_dfs[sdays_name]['prediction'], args.plot)
-print('nreg')
-utils.print_metrics(demand_dfs[sdays_name]['demand'], demand_dfs[nreg_name]['prediction'], args.plot)
-print('regl')
-utils.print_metrics(demand_dfs[sdays_name]['demand'], demand_dfs[regl_name]['prediction'], args.plot)
-print('regm')
-utils.print_metrics(demand_dfs[sdays_name]['demand'], demand_dfs[regm_name]['prediction'], args.plot)
+for name, pdf in demand_dfs.items():
+    print(name)
+    utils.print_metrics(pdf['demand'], pdf['prediction'], args.plot)
